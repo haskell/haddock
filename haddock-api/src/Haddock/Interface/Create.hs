f25edf0f4ce225981421d634ff55aa4562d50ab9
alexbiehl <alex.biehl@gmail.com>
2017-10-07 10:57:21 +0200

    Split avail if declaration is not exported itself

--- haddock-api/src/Haddock/Interface/Create.hs
+++ haddock-api/src/Haddock/Interface/Create.hs
@@ -655,9 +655,16 @@ mkExportItems
     lookupExport (_, avails) =
       concat <$> traverse availExport (nubAvails avails)
 
-    availExport avail =
-      availExportItem is_sig modMap thisMod semMod warnings exportedNames
-        maps fixMap splices instIfaceMap dflags avail
+    availExport avail
+      | availName avail `notElem` availNamesWithSelectors avail = do
+          exportItems <- for (availNamesWithSelectors avail)
+                             (availExportItem is_sig modMap thisMod semMod
+                               warnings exportedNames maps fixMap splices
+                               instIfaceMap dflags . Avail.avail)
+          return (concat exportItems)
+      | otherwise =
+          availExportItem is_sig modMap thisMod semMod warnings exportedNames
+            maps fixMap splices instIfaceMap dflags avail
 
 availExportItem :: Bool               -- is it a signature
                 -> IfaceMap
