88780b6e5083e57d9fd81ead70569beac91362c1
Hécate Moonlight <hecate+github@glitchbra.in>
2021-05-11 10:14:47 +0200

    Revert "Removal of HsVersions.h (#1388)"
    This reverts commit 72118896464f94d81f10c52f5d9261efcacc57a6.

--- haddock-api/src/Haddock/Convert.hs
+++ haddock-api/src/Haddock/Convert.hs
@@ -49,8 +49,9 @@ import GHC.Builtin.Types ( eqTyConName, listTyConName, liftedTypeKindTyConName
 import GHC.Builtin.Names ( hasKey, eqTyConKey, ipClassKey, tYPETyConKey
                  , liftedDataConKey, boxedRepDataConKey )
 import GHC.Types.Unique ( getUnique )
-import GHC.Utils.Misc ( chkAppend, dropList, equalLength
-                      , filterByList, filterOut, debugIsOn )
+import GHC.Utils.Misc ( chkAppend, debugIsOn, dropList, equalLength
+                      , filterByList, filterOut )
+import GHC.Utils.Panic ( assertPanic )
 import GHC.Types.Var
 import GHC.Types.Var.Set
 import GHC.Types.SrcLoc
@@ -935,8 +936,8 @@ tcSplitForAllTysReqPreserveSynonyms :: Type -> ([ReqTVBinder], Type)
 tcSplitForAllTysReqPreserveSynonyms ty =
   let (all_bndrs, body) = tcSplitSomeForAllTysPreserveSynonyms isVisibleArgFlag ty
       req_bndrs         = mapMaybe mk_req_bndr_maybe all_bndrs in
-  ASSERT( req_bndrs `equalLength` all_bndrs)
-    (req_bndrs, body)
+  ASSERT( req_bndrs `equalLength` all_bndrs )
+  (req_bndrs, body)
   where
     mk_req_bndr_maybe :: TyCoVarBinder -> Maybe ReqTVBinder
     mk_req_bndr_maybe (Bndr tv argf) = case argf of
