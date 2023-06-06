bfb27b7434f652d8dff5b6572b5215eeb0b4456f
Ari Fordsham <arifordsham@gmail.com>
2023-06-07 01:44:18 +0300

    Tune

--- goldens.sh
+++ goldens.sh
@@ -4,5 +4,5 @@ cabal test html-test hypsrc-test --test-option=--accept
 cp -r html-test/ref/* goldens/new
 cp -r hypsrc-test/ref/src/* goldens/new/src
 
-npx --prefix goldens prettier --write goldens
+npx --prefix goldens prettier --write goldens/new
 diff -r goldens/old goldens/new > goldens.diff
