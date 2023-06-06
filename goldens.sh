a3ed8c8de412d380a1f50107ab6106a492e6da33
Ari Fordsham <arifordsham@gmail.com>
2023-06-06 03:36:12 +0300

    Add html refs

--- goldens.sh
+++ goldens.sh
@@ -2,5 +2,5 @@ cabal test html-test hypsrc-test --test-option=--accept
 cp -r html-test/ref goldens/new
 cp -r hypsrc-test/ref/src goldens/new/src
 
-npx prettier --write goldens
+npx --prefix goldens prettier --write goldens
 diff -r goldens/old goldens/new > goldens.diff
