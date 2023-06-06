5ec9f456ef664dd6c2973a8afdd8411b32565f92
Ari Fordsham <arifordsham@gmail.com>
2023-06-06 04:10:58 +0300

    Fix list

--- goldens.sh
+++ goldens.sh
@@ -1,6 +1,6 @@
 cabal test html-test hypsrc-test --test-option=--accept
-cp -r html-test/ref goldens/new
-cp -r hypsrc-test/ref/src goldens/new/src
+cp -r html-test/ref/* goldens/new
+cp -r hypsrc-test/ref/src/* goldens/new/src
 
 npx --prefix goldens prettier --write goldens
 diff -r goldens/old goldens/new > goldens.diff
