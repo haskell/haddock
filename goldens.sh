86dd4eecbcf5e1496901bbd8a067a9ef85916584
Ari Fordsham <arifordsham@gmail.com>
2023-06-07 06:51:19 +0300

    Clean up

--- goldens.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-cabal test html-test hypsrc-test --test-option=--accept
-cp -r html-test/ref/* goldens/new
-cp -r hypsrc-test/ref/src/* goldens/new/src
-
-npx --prefix goldens prettier --write goldens
-diff -r goldens/old goldens/new > goldens.diff
