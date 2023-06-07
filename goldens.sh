712f2119e3a00e00cd4a6b3e4e9168ee595a862f
Ari Fordsham <arifordsham@gmail.com>
2023-06-07 06:37:58 +0300

    Almost there

--- goldens.sh
+++ goldens.sh
@@ -4,5 +4,5 @@ cabal test html-test hypsrc-test --test-option=--accept
 cp -r html-test/ref/* goldens/new
 cp -r hypsrc-test/ref/src/* goldens/new/src
 
-npx --prefix goldens prettier --write goldens/new
+npx --prefix goldens prettier --write goldens
 diff -r goldens/old goldens/new > goldens.diff
